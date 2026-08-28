c613a18b84929a4557bcfb13e86677fac81dd4ca
umarxdev7 <umarkhanjamirkhan9970@gmail.com>
2026-08-28 17:40:32 +0000

    Comparison In Java

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,29 @@
+console.log(2 > 2);
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+// best comaprison because it is 
+//between two same datatypes
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// === 
+
+console.log("2" === 2);
+
+// Avoid conversions of different
+// datatypes. Means between two 
+// different data types
