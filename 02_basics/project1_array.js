0a085f32fc9d00cbbf87a1190a4fb4bb79866a30
umarxdev7 <umarkhanjamirkhan9970@gmail.com>
2026-09-13 14:03:34 +0000

    Array Projects

---
+++ 02_basics/project1_array.js
@@ -0,0 +1,105 @@
+/*let portugal = [
+    "Ronaldo",
+    "Bruno Fernandes",
+    "Bernardo Silva",
+    "Rafael Leao"
+]
+portugal.push("Vitinha")
+
+portugal.splice(3, 1)
+
+portugal.unshift("Diogo Costa")
+
+let finalSquad = [...portugal]
+
+console.log(portugal.includes("Ronaldo"));
+console.log(portugal.indexOf("Ronaldo"));
+
+console.log(finalSquad);
+
+/***************************************************************** */
+
+/*let squad = [
+    "Ronaldo",
+    "Bruno",
+    "Bernardo",
+    "Leao",
+    "Vitinha"
+]
+squad.splice(1, 0, "Palhinha")
+squad.splice(3, 1)
+squad.unshift("Nuno Mendes")
+squad.pop()
+console.log(squad.includes("Ronaldo"));
+let finalSquad = [...squad]
+console.log(finalSquad);
+*/
+
+
+/************PORTUGAL MATCH SQUAD MANAGER**************** */
+
+/*let squad = [
+    "Ronaldo",
+    "Bruno",
+    "Bernardo",
+    "Leao",
+    "Vitinha"
+]
+squad.splice(1, 0, "Cancelo")
+squad.splice(3, 1)
+squad.unshift("Diogo Costa")
+squad.push("Pedro Neto")
+console.log(squad.includes("Ronaldo"));
+console.log(squad.indexOf("Ronaldo"));
+let finalSquad = [...squad]
+console.log(finalSquad);
+
+*/
+
+
+
+/************************Portugal Squad Manager — Mega Project******************** */
+
+/*let squad = [
+    "Ronaldo",
+    "Bruno Fernandes",
+    "Bernardo Silva",
+    "Rafael Leao",
+    "Vitinha"
+]
+squad.unshift("Diogo Costa")
+squad.splice(4, 0, "Joao Felix") //ab  0 pe costa hai
+squad.splice(5, 1)
+
+
+squad.push("Pedro Neto")
+console.log(squad.includes("Ronaldo"));
+console.log(squad.indexOf("Ronaldo"));
+let finalSquad = [...squad]
+console.log(finalSquad);
+
+*/
+
+
+
+/************************CHALLENGE************************ */
+
+let squad = [
+    "Ronaldo",
+    "Bruno",
+    "Bernardo",
+    "Leao",
+    "Vitinha",
+    "Palhinha"
+]
+squad.unshift("Diogo Costa")
+squad.splice(4, 1)
+squad.splice(3, 0, "Nuno Mendes")
+squad.push("Ruben Dias")
+console.log(squad.includes("Ronaldo"));
+console.log(squad.indexOf("Ronaldo"));
+let finalSquad = [...squad]
+console.log(finalSquad);
+
+
+
